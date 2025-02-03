34f96894b2062ab31cf007f525e7df4e5bc9f11e
Lor-Lor <ladrianpalmares@gmail.com>
2025-02-03 09:55:35 +0800

    lor
    commit2

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+$servername= "localhost";
+$username = "root";
+$password =" ";
+$database = "samplelogin";
+
+//create connection
+$conn = new mysqli($servername, $username,  $password, $database);
+
+if($conn -> connect_error){
+    die("Connection Failed" . $conn -> connect_server);
+}
+?>
