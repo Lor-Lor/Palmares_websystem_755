ce89dcc778c2b7ad1536513a8bdcc4cfe8d5a43d
Lor-Lor <ladrianpalmares@gmail.com>
2025-02-03 10:00:20 +0800

    lor3
    commit3

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
 $servername= "localhost";
 $username = "root";
-$password =" ";
-$database = "samplelogin";
+$password ="";
+$database ="samplelogin";
 
 //create connection
 $conn = new mysqli($servername, $username,  $password, $database);
